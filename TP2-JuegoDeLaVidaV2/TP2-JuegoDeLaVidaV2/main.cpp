368ba631a44c8c2ce0fed0c38cd6235875f72d44
Andres Seivane <tu@correo.com>
2022-10-22 17:35:38 -0300

    Templates to headers and vscode tasks

--- TP2-JuegoDeLaVidaV2/TP2-JuegoDeLaVidaV2/main.cpp
+++ TP2-JuegoDeLaVidaV2/TP2-JuegoDeLaVidaV2/main.cpp
@@ -1,13 +1,15 @@
 #include "Tablero.h"
+//#include "Tablero.cpp"
+//#include "Lista.h"
+//#include "Lista.cpp"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-
     Tablero * tab = new Tablero(3,3,3);
 
-    cout << tab->casillero(1,2,1) << endl;
+    //cout << tab->casillero(1,2,1) << endl;
 
     delete tab;
 
