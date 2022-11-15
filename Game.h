13f8a3eaaccffb880881be59d39b36e21609d37d
Matias <mjannello@gmail.com>
2022-11-14 22:19:09 -0300

    Add bitmap functionality

---
+++ Game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Board.h"
+#include "Cell.h"
+
+class Game {
+private:
+	Board<Cell*>* board;
+public:
+	Game();
+	~Game();
+	void askBoardSize();
+	Board<Cell*>* getBoard();
+	void setBoard(int width, int large, int height);
+	void askCellStateLimits();
+	void nextRound();
+	
+	void setConfigOne();
+	
+};
+
+#endif
